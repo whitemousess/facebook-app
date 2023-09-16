ee1ff20ff187114f1456b6c52d16d6b968f78ff9
whitemousess <thangcoiofficial@gmail.com>
2023-09-16 16:28:41 +0700

    update

--- src/screens/NotificationScreen.js
+++ src/screens/NotificationScreen.js
@@ -1,11 +1,21 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 function NotificationScreen() {
   return (
-    <View>
-      <Text>NotificationScreen</Text>
+    <View style={styles.container}>
+      <Text style={{ color: "white" }}>NotificationScreen</Text>
+      <Text style={{ color: "white" }}>coming soon !</Text>
     </View>
   );
 }
 
 export default NotificationScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#1c1c1c",
+  },
+});
--- src/screens/VideoScreen.js
+++ src/screens/VideoScreen.js
@@ -1,11 +1,21 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 function VideoScreen() {
   return (
-    <View>
-      <Text>VideoScreen</Text>
+    <View style={styles.container}>
+      <Text style={{color: "white"}}>VideoScreen</Text>
+      <Text style={{ color: "white" }}>coming soon !</Text>
     </View>
   );
 }
 
 export default VideoScreen;
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: "#1c1c1c",
+  }
+})
